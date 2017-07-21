3a7872d26593852c8aeca0463bb62d8f5accbccf
Sage Kelly <musicaltech120@bellsouth.net>
2017-07-21 09:03:01 -0400

    Black Noice Updates
    Moving data from an old computer

---
+++ BlackNoise/BlackNoise/TimerData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace BlackNoise
+{
+    public class TimerData
+    {
+        /// <summary>
+        /// The ID of the notification
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// The name of the Notification
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The interval (stored in milliseconds) of the 
+        /// </summary>
+        public int TimerInterval { get; set; }
+
+        /// <summary>
+        /// The sound the alarm will make when it triggers
+        /// </summary>
+        public MediaElement Chime { get; set; }
+    }
+}
